369e179979550e303ae618127a5859f24d6485e6
zirax007 <jamal.et.tousy@gmail.com>
2020-03-26 16:34:37 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,160 @@
+$(document).ready(function () {
+
+    $('#preloader').delay(500).fadeOut('slow'); 
+
+    jQuery('#grid-container').on('initComplete.cbp', function () {
+        if ($('#ajax-tab-container').length) {
+            $('#ajax-tab-container').easytabs({
+                tabs: 'header nav ul li'
+            });
+        }
+    });
+
+    function transform(){
+        var outdiv = '<div class="menuout"><div class="menuin"><ul class="tabs"></ul></div></div>';
+        $(outdiv).appendTo("nav");
+        var resmenus = $('.tabs').html();
+        $(".menuout .menuin .tabs").append(resmenus);
+       $('.menuin').hide(); 
+    }
+    transform();
+    $('.hamburger').on('click', function() {
+       $('.menuin').slideToggle(); 
+    });
+    $('.menuout').on('click', function () {
+        $('.menuin').slideUp();  
+    });
+
+    if ($('.owl-carousel').length) {
+        $('.owl-carousel').each(function () {
+            $(this).owlCarousel({
+                items: $(this).data('items') ? $(this).data('items') : 3
+                , autoPlay: $(this).data('autoplay') ? $(this).data('autoplay') : 2500
+                , pagination: $(this).data('pagination') ? $(this).data('pagination') : false
+                , itemsDesktop: [1199, 3]
+                , itemsDesktopSmall: [979, 3]
+            });
+        });
+    }
+
+ 
+    $('#grid-container').cubeportfolio({
+        layoutMode: 'grid',
+        filters: '#filters-container',
+        gridAdjustment: 'responsive',
+        animationType: 'skew',
+        defaultFilter: '*',
+        gapVertical: 0,
+        gapHorizontal: 0,
+        singlePageAnimation: 'fade',
+        mediaQueries: [{
+                width: 700,
+                cols: 3,
+            }, {
+                width: 480,
+                cols: 2,
+                options: {
+                    caption: '',
+                    gapHorizontal: 30,
+                    gapVertical: 20,
+                }
+            }, {
+                width: 320,
+                cols: 1,
+                options: {
+                    caption: '',
+                    gapHorizontal: 50,
+                }
+            }],            
+        singlePageCallback: function (url, element) {
+            var t = this;
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 30000
+                })
+                .done(function (result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function () {
+                    t.updateSinglePage('AJAX Error! Please refresh the page!');
+                });
+        },
+            plugins: {
+                loadMore: {
+                    element: '#js-loadMore-agency',
+                    action: 'click',
+                    loadItems: 3,
+                }
+            }
+    }); 
+
+       $('#grid-blog').cubeportfolio({
+        layoutMode: 'grid',
+        gridAdjustment: 'responsive',
+        gapVertical: 0,
+        gapHorizontal: 0,
+        mediaQueries: [{
+                width: 700,
+                cols: 3,
+            }, {
+                width: 480,
+                cols: 2,
+                options: {
+                    caption: '',
+                    gapHorizontal: 30,
+                    gapVertical: 20,
+                }
+            }, {
+                width: 320,
+                cols: 1,
+                options: {
+                    caption: '',
+                    gapHorizontal: 50,
+                }
+            }],
+            plugins: {
+                loadMore: {
+                    element: '#load-posts',
+                    action: 'click',
+                    loadItems: 3,
+                }
+            }
+    }); 
+
+  
+    $('#widget-gallery').cubeportfolio({
+        layoutMode: 'grid',
+        gridAdjustment: 'responsive',
+        gapVertical: 0,
+        gapHorizontal: 0,
+        mediaQueries: [{
+                width: 700,
+                cols: 4,
+            }, {
+                width: 480,
+                cols: 2,
+                options: {
+                    caption: '',
+                    gapHorizontal: 30,
+                    gapVertical: 20,
+                }
+            }, {
+                width: 320,
+                cols: 1,
+                options: {
+                    caption: '',
+                    gapHorizontal: 50,
+                }
+            }]
+    }); 
+
+}); 
+
+
+"use strict";
+$(window).load(function () {
+
+
+}); 
